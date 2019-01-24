9bdc98de27f8758ccf5f2cf735dae475ed48c397
Heidar Bernhardsson <heidarbn@gmail.com>
2019-01-24 10:53:29 +0000

    add procedure testing example with ruby-plsql-spec

--- init.sql
+++ init.sql
@@ -3,6 +3,7 @@ GRANT CREATE ANY PROCEDURE TO ruby;
 GRANT CREATE ANY INDEX TO ruby;
 GRANT CREATE ANY TABLE TO ruby;
 GRANT CONNECT TO ruby;
+GRANT UNLIMITED TABLESPACE TO ruby;
 
 CREATE OR REPLACE FUNCTION ruby.betwnstr (
    string_in   IN   VARCHAR2,
@@ -18,5 +19,34 @@ BEGIN
       l_start := 1;
    END IF;
    RETURN (SUBSTR (string_in, l_start, end_in - l_start + 1));
+END;
+/
+
+CREATE TABLE ruby.rooms (
+  room_key NUMBER PRIMARY KEY,
+  name VARCHAR2(100)
+)
+/
+
+CREATE TABLE ruby.room_contents (
+  contents_key NUMBER PRIMARY KEY,
+  room_key NUMBER, name VARCHAR2(100)
+)
+/
+
+ALTER TABLE ruby.room_contents ADD CONSTRAINT
+  fk_rooms FOREIGN KEY (room_key)
+  REFERENCES ruby.rooms (room_key)
+/
+
+CREATE OR REPLACE PROCEDURE ruby.remove_rooms_by_name (
+  name_in IN ruby.rooms.name%TYPE)
+IS
+BEGIN
+  IF NAME_IN IS NULL
+  THEN
+    RAISE PROGRAM_ERROR;
+  END IF;
+  DELETE FROM ruby.rooms WHERE name LIKE name_in;
 END;
 /
